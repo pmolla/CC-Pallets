19dd06439ce63cc20d7c3b4a897460bdb9b44c41
pmolla <pablo.molla.ar@gmail.com>
2024-12-20 09:32:24 -0300

    Update

--- __manifest__.py
+++ __manifest__.py
@@ -4,7 +4,7 @@
     'author': 'Pablo Molla',
     'category': 'Custom',
     'summary': 'Portal View for Cuenta Corriente',
-    'depends': ['portal'],  # Replace with the actual Studio module name
+    'depends': ['base','portal'],  # Replace with the actual Studio module name
     'data': [
         'views/portal_templates.xml',
         'security/ir.model.access.csv',
