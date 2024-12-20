097b616a33c7f3f6ef1e4188b88da029d3fe9c91
pmolla <pablo.molla.ar@gmail.com>
2024-12-20 17:56:36 -0300

    update

--- __manifest__.py
+++ __manifest__.py
@@ -2,12 +2,14 @@
     'name': 'Cuenta Corriente Portal',
     'version': '1.0',
     'depends': ['base', 'portal', 'website'],
+
     'data': [
         'security/ir.model.access.csv',
         'security/ir.rule.xml',
-        'views/portal_menu.xml',
         'views/portal_templates.xml',
+        'views/portal_menu.xml',  # Load this after related dependencies
     ],
+
     'installable': True,
     'application': False,
 }
