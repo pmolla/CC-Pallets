022cca64eb88daa01704a5b9bd162bdb6cd0add3
pmolla <pablo.molla.ar@gmail.com>
2024-12-24 13:29:25 -0300

    update

--- controllers/portal.py
+++ controllers/portal.py
@@ -2,18 +2,6 @@ from odoo import http
 from odoo.http import request
 
 class CuentaCorriente(http.Controller):
-
-    @http.route('/my/cuenta_corriente', type='http', auth='user', website=True)
-    def cuenta_corriente(self, **kwargs):
-        """Render the Cuenta Corriente page."""
-        # Buscar los registros de 'x_cuenta_corriente' vinculados al usuario actual
-        records = request.env['x_cuenta_corriente'].sudo().search([
-            ('x_studio_cliente', '=', request.env.user.partner_id.id)
-        ])
-        return request.render('cuentacorriente.portal_cuenta_corriente_list', {
-            'records': records
-        })
-
     @http.route('/my/cuenta_corriente/movements', type='http', auth='user', website=True)
     def cuenta_corriente_movements(self, **kwargs):
         """Render the Inventory Movements page for the Cuenta Corriente."""
