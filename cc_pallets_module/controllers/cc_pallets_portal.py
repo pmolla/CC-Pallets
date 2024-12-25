c7e6477c025d8548394cbe99fe0a3a0d1599823a
pmolla <pablo.molla.ar@gmail.com>
2024-12-25 17:38:44 -0300

    updt

--- cc_pallets_module/controllers/cc_pallets_portal.py
+++ cc_pallets_module/controllers/cc_pallets_portal.py
@@ -2,18 +2,25 @@ from odoo import http
 from odoo.http import request
 
 class CCPalletsPortal(http.Controller):
-    
-    @http.route(['/my/cc_pallets'], type='http', auth="user", website=True)
-    def portal_cc_pallets(self, **kw):
-        user = request.env.user
-        pallets = request.env['cc.pallets.model'].sudo().search([('contact_id', '=', user.partner_id.id)])
-        return request.render('cc_pallets_module.portal_cc_pallets_list', {
-            'cc_pallets': pallets,
+
+    @http.route(['/my/cc_pallets'], type='http', auth='user', website=True)
+    def portal_cc_pallets_list(self, **kwargs):
+        """Controlador para listar los CC Pallets en el portal."""
+        # Obtener los pallets asociados al usuario actual
+        cc_pallets = request.env['cc.pallets'].search([('partner_id', '=', request.env.user.partner_id.id)])
+        
+        return request.render('your_module_name.cc_pallets_portal_page', {
+            'cc_pallets': cc_pallets
         })
 
-    @http.route(['/my/cc_pallets/<int:pallet_id>'], type='http', auth="user", website=True)
-    def portal_cc_pallets_view(self, pallet_id, **kw):
-        pallet = request.env['cc.pallets.model'].sudo().browse(pallet_id)
-        return request.render('cc_pallets_module.cc_pallets_portal_view', {
-            'cc_pallet': pallet,
+    @http.route(['/my/cc_pallets/<int:pallet_id>'], type='http', auth='user', website=True)
+    def portal_cc_pallet_view(self, pallet_id, **kwargs):
+        """Controlador para ver los detalles de un CC Pallet."""
+        # Buscar el pallet por ID y asegurar que pertenece al usuario actual
+        pallet = request.env['cc.pallets'].sudo().browse(pallet_id)
+        if not pallet or pallet.partner_id.id != request.env.user.partner_id.id:
+            return request.render('website.404')
+        
+        return request.render('your_module_name.cc_pallets_portal_view', {
+            'cc_pallet': pallet
         })
