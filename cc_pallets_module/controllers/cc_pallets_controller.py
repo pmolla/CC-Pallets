0bbe10e6f279d8d11fec0cc26a7ae735882140af
pmolla <pablo.molla.ar@gmail.com>
2024-12-25 21:11:19 -0300

    update

--- cc_pallets_module/controllers/cc_pallets_controller.py
+++ cc_pallets_module/controllers/cc_pallets_controller.py
@@ -7,7 +7,7 @@ class CCPalletsPortal(http.Controller):
     def portal_cc_pallets_list(self, **kwargs):
         """Controlador para listar los CC Pallets en el portal."""
         # Obtener los pallets asociados al usuario actual
-        cc_pallets = request.env['cc.pallets'].search([('partner_id', '=', request.env.user.partner_id.id)])
+        cc_pallets = request.env['cc.pallets.model'].search([('contact_id', '=', request.env.user.partner_id.id)])
         
         return request.render('cc_pallets_module.cc_pallets_portal_page', {
             'cc_pallets': cc_pallets
@@ -17,8 +17,8 @@ class CCPalletsPortal(http.Controller):
     def portal_cc_pallet_view(self, pallet_id, **kwargs):
         """Controlador para ver los detalles de un CC Pallet."""
         # Buscar el pallet por ID y asegurar que pertenece al usuario actual
-        pallet = request.env['cc.pallets'].sudo().browse(pallet_id)
-        if not pallet or pallet.partner_id.id != request.env.user.partner_id.id:
+        pallet = request.env['cc.pallets.model'].sudo().browse(pallet_id)
+        if not pallet or pallet.contact_id.id != request.env.user.partner_id.id:
             return request.render('website.404')
         
         return request.render('cc_pallets_module.cc_pallets_portal_view', {
